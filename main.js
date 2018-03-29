b6012f53126c30ca37e9064eb9202a2d697d97f4
Jayfiled <jayfiled@gmail.com>
2018-03-29 13:40:18 +1100

    Set tip button to countdown and change when no more tips

--- main.js
+++ main.js
@@ -40,64 +40,27 @@ function generateTip() {
    var tip = tipsList[generateNumber()];
     var tipElement = document.querySelector('.js-tip');
     var tipLimitCount = document.querySelector('.tip-limit-count');
+    
     tipElement.innerHTML = tip;
     tipLimitCount.innerHTML = tipLimit;
 }
 
-
-generateTip();
-
-
-
-
-
 function onTipButtonClick() {
     var tipButton = document.querySelector('.tip-button');
     tipButton.addEventListener('click', function() {
-        console.log("Rip Clippy");
-})
+        tipLimit = tipLimit - 1;
+        if (tipLimit >= 0) {
+            generateTip();
+            
+            if (tipLimit === 0) {
+                tipButton.innerHTML = "See you in another tab!";
+                tipButton.classList.add('disabled');
+            }
+        }
+});
 }
+
 onTipButtonClick();
 
 
-/*
-// Tip Limit counter
-var tipLimit = 3;
-
-// Generate a number
-function generateNumber() {
-  ...
-}
-
-// Generate a tip:
-// 1. Get random number from generateNumber()
-// 2. Use the random number to get the tip from the array
-// 3. Show the tip
-function generateTip() {
-  var tip = tipsList[generateNumber()];
-  var tipElement = document.querySelector('.js-tip');
-  var tipLimitCount = document.querySelector('.tip-limit-count');
-
-  tipElement.innerHTML = tip;
-  tipLimitCount.innerHTML = tipLimit;
-}
-  
-...
-
-// Get the first tip
 generateTip();
-
-
-
-// Tip button click
-// 1. Select the tip button
-// 2. Add a click event listener
-// 3. When the button is clicked:
-// 3a. Subtract 1 from the tipLimit
-// 3b. If the tipLimit is still above or equal to 0, generate a new tip
-// 3c. If not, change the button text and look
-
-
-// Get the first tip
-
-*/
